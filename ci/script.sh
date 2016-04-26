450f4e290042f145f7b1a56c73d09b712ec1548a
Jorge Aparicio <japaricious@gmail.com>
2016-04-25 23:03:24 -0500

    set CC and AR variables before testing

--- ci/script.sh
+++ ci/script.sh
@@ -3,17 +3,23 @@ set -ex
 . $(dirname $0)/utils.sh
 
 main() {
+    local prefix=
     case $TARGET in
         thumbv7m-none-eabi)
-            sudo apt-get install -y --force-yes --no-install-recommends \
-                 gcc-arm-none-eabi libnewlib-dev
+            prefix=arm-none-eabi-
             ;;
         x86_64-unknown-linux-gnu)
+            prefix=
             ;;
         *)
             die "unhandled target: $TARGET"
             ;;
     esac
+
+    export AR_${TARGET//-/_}=${prefix}ar
+    export CC_${TARGET//-/_}=${prefix}gcc
+
+    cargo build --target $TARGET
 }
 
 main
