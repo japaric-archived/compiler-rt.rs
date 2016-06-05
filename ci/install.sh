951ba2bf49b953e3f13a6ced39ffdb6350c14601
Jorge Aparicio <japaricious@gmail.com>
2016-06-05 17:14:43 -0500

    use gcc-5 for cortex-m7 targets

--- ci/install.sh
+++ ci/install.sh
@@ -4,6 +4,11 @@ set -ex
 
 main() {
     case $TARGET in
+        cortex-m7*)
+            curl -sL https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q1-update/+download/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2 | \
+                sudo tar --strip-components 1 -C /usr/local -xj
+            sudo apt-get install -y --force-yes --no-install-recommends libc6-i386
+            ;;
         cortex-m* | no-linker-field | thumbv*)
             sudo apt-get install -y --force-yes --no-install-recommends \
                  gcc-arm-none-eabi libnewlib-dev
